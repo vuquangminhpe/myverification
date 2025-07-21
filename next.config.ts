06ebc79f8d1ff2f70d904c69a826e2a452ae7ad9
vuquangminhpe <minhvqhe176726@fpt.edu.vn>
2025-07-21 11:39:37 +0700

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["lightningcss"],
+  serverExternalPackages: ["lightningcss"],
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (isServer) {
+      config.externals.push("lightningcss");
+    }
+    return config;
   },
 };
 
